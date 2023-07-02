304597a2e988ca870fb3e67fc7d351dac1145cda
Mehdi Bourahla <mehdibrhl4@gmail.com>
2023-07-02 12:12:59 -0500

    🐛 Fix YAMNet TV detector

--- yamnet/yamnet_inference/yamnet_pretrained.py
+++ yamnet/yamnet_inference/yamnet_pretrained.py
@@ -3,7 +3,9 @@ import tensorflow as tf
 from pathlib import Path
 import argparse
 import os
-import librosa
+import resampy
+import numpy as np
+import soundfile as sf
 import logging
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
@@ -36,23 +38,40 @@ def initialize_args(parser):
     )
 
 
-def get_ear_data(path):
+def load_ground_truth(gt_dir):
     # Get the ear data
-    ear_data = pd.read_csv(path)
-    ear_data["Tv"] = ear_data["Tv"].astype(int)
+    ear_data = pd.read_csv(gt_dir)
 
-    # Log the number of records before filtering
-    logging.info(f"Number of records before filtering: {len(ear_data)}")
+    # Convert all column names to lowercase
+    ear_data.columns = map(str.lower, ear_data.columns)
 
-    # Group by FileName and keep only records where coders agree on "Tv" column
-    agreed_data = ear_data.groupby("FileName").filter(lambda x: x["Tv"].nunique() == 1)
+    ear_data["tv"] = ear_data["tv"].replace(r"^\s*$", "0", regex=True)
+    ear_data["tv"] = ear_data["tv"].fillna("0")
+    ear_data["tv"] = ear_data["tv"].astype(int)
 
-    # Log the number of records after filtering
-    logging.info(f"Number of records after filtering: {len(agreed_data)}")
+    # Keep only records where coders agree on "tv" column
+    agreed_data = ear_data.groupby("filename").filter(lambda x: x["tv"].nunique() == 1)
 
-    # Set the index to be the FileName
-    agreed_data.set_index("FileName", inplace=True)
-    return agreed_data
+    # Drop duplicates based on FileName, keep the first record
+    agreed_data = agreed_data.drop_duplicates(subset="filename", keep="first")
+
+    # Split the data into two groups based on the value of "tv"
+    tv_0 = agreed_data[agreed_data["tv"] == 0]
+    tv_1 = agreed_data[agreed_data["tv"] == 1]
+
+    # Find out which group is larger
+    larger_group = tv_0 if len(tv_0) > len(tv_1) else tv_1
+    smaller_group = tv_1 if larger_group is tv_0 else tv_0
+
+    # Randomly sample from the larger group to match the size of the smaller group
+    larger_group = larger_group.sample(len(smaller_group), random_state=42)
+    logging.info(f"Total data: {len(larger_group) + len(smaller_group)}")
+
+    # Concatenate the balanced data
+    balanced_data = pd.concat([larger_group, smaller_group])
+    balanced_data.set_index("filename", inplace=True)
+
+    return balanced_data
 
 
 def yamnet_tv(audio_wav, yamnet_model):
@@ -72,17 +91,30 @@ def yamnet_tv(audio_wav, yamnet_model):
 def process_audio(path, ground_truth, yamnet_model):
     logging.info(f"Processing audio: {path}")
     try:
-        audio_wav, _ = librosa.load(path, sr=16000)
+        wav_data, sr = sf.read(path, dtype=np.int16)
     except Exception as e:
         logging.error(f"Error loading audio file {path}: {e}")
         return None
 
-    filename = path.split("/")[-1]
-    record_series = ground_truth.loc[filename].iloc[0]
-    record = record_series.to_dict()
-    record["FileName"] = record_series.name
+    assert wav_data.dtype == np.int16, "Bad sample type: %r" % wav_data.dtype
+    waveform = wav_data / 32768.0  # Convert to [-1.0, +1.0]
+    waveform = waveform.astype("float32")
 
-    yamnet_scores = yamnet_tv(audio_wav, yamnet_model)
+    # Convert to mono and the sample rate expected by YAMNet.
+    if len(waveform.shape) > 1:
+        waveform = np.mean(waveform, axis=1)
+    if sr != 16000:
+        waveform = resampy.resample(waveform, sr, 16000)
+
+    filename = path.split("/")[-1]
+    # Check if the filename is in the ground truth dataframe
+    if filename not in ground_truth.index.tolist():
+        return None
+    record = {"filename": filename, "isTV": ground_truth.loc[filename]["tv"]}
+
+    yamnet_scores = yamnet_tv(waveform, yamnet_model)
+
+    # Create a dictionary to store the results
     record["yamnet_tv"] = yamnet_scores["tv"]
     record["yamnet_radio"] = yamnet_scores["radio"]
 
@@ -100,15 +132,15 @@ def process_audio_wrapper(args):
 
 def process_audio_df(audio_dir, gt_dir, output_dir):
     yamnet_model = hub.load("https://tfhub.dev/google/yamnet/1")
-    ground_truth = get_ear_data(gt_dir)
+    ground_truth = load_ground_truth(gt_dir)
     data_path = Path(audio_dir)
     data = list(data_path.glob("*.wav"))
 
     records = []
     processed_count = 0
-    tv_count = 0  # Add a variable to keep track of Tv labeled audio clips
+    tv_count = 0
 
-    with ThreadPoolExecutor(max_workers=16) as executor:
+    with ThreadPoolExecutor() as executor:
         args_list = [(file, ground_truth, yamnet_model) for file in data]
         results = executor.map(process_audio_wrapper, args_list)
 
@@ -117,11 +149,8 @@ def process_audio_df(audio_dir, gt_dir, output_dir):
             if record is not None:
                 records.append(record)
                 processed_count += 1
-                if record["Tv"] == 1:  # Update the tv_count if Tv label is 1
+                if record["isTV"] == 1:
                     tv_count += 1
-            logging.info(
-                f"Processed {i}/{len(data)} audio files. Successful: {processed_count}. Tv clips: {tv_count}"
-            )
 
     logging.info(
         f"Processed {processed_count}/{len(data)} audio files. Tv clips: {tv_count}"
@@ -143,6 +172,7 @@ def main(args):
     gt_dir = args.gt_dir
     output_dir = args.output_dir
 
+    os.makedirs(output_dir, exist_ok=True)
     process_audio_df(audio_dir, gt_dir, output_dir)
 
     logging.info("Finished processing.")
